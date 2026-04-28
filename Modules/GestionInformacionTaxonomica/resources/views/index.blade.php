bc7df5dd21e72ece8a016c35bfe049a06e15210e
Paul Cajas <paul.cajas@epn.edu.ec>
2026-04-28 11:07:59 -0500

    refactor(taxonomia): "consolida módulo GestionInformacionTaxonomica en InventarioGestionColeccion"
    - Mueve los 14 contextos Behat y 14 features al módulo correcto
    - Actualiza namespaces de GestionInformacionTaxonomica a InventarioGestionColeccion
    - Agrega diagrama ER preliminar del módulo
    - Elimina el módulo GestionInformacionTaxonomica duplicado

--- Modules/GestionInformacionTaxonomica/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-gestionprestamosrecepciones::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('gestionprestamosrecepciones.name') !!}</p>
-</x-gestionprestamosrecepciones::layouts.master>
