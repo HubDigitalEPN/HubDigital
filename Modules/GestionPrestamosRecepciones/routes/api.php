db5bfec60dd944a7d152fc53fec6e72f7007b429
JotaValla <jimmy.valladares@epn.edu.ec>
2026-05-11 15:40:10 -0500

    fix(prestamos): "Se elimino los endpoints de la API de la feature envio de solicitud"

--- Modules/GestionPrestamosRecepciones/routes/api.php
+++ Modules/GestionPrestamosRecepciones/routes/api.php
@@ -1,18 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\GestionPrestamosRecepciones\Presentation\Http\Controllers\SolicitudPrestamoController;
-
-Route::prefix('v1')->group(function () {
-    Route::post('solicitudes-prestamo', [SolicitudPrestamoController::class, 'store'])
-        ->name('solicitudes-prestamo.store');
-
-    Route::put('solicitudes-prestamo/{id}', [SolicitudPrestamoController::class, 'update'])
-        ->name('solicitudes-prestamo.update');
-
-    Route::post('solicitudes-prestamo/{id}/envios', [SolicitudPrestamoController::class, 'enviar'])
-        ->name('solicitudes-prestamo.enviar');
-
-    Route::post('solicitudes-prestamo/{id}/actas-firmadas', [SolicitudPrestamoController::class, 'subirActa'])
-        ->name('solicitudes-prestamo.subir-acta');
-});
